278f29401a43b3ae851f94074df62ab0aca00850
rekan <rekanagy06@gmail.com>
2017-04-25 10:13:26 +0200

    Create base project

---
+++ src/main/java/com/wordcloudwonders/WordcloudwondersApplication.java
@@ -0,0 +1,12 @@
+package com.wordcloudwonders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcloudwondersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcloudwondersApplication.class, args);
+	}
+}
