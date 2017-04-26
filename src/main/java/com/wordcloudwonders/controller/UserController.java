3a3dea6ad8d642cf296d9b6b62c3cf549b498613
cickib <bellakocsis06@gmail.com>
2017-04-26 10:31:13 +0200

    user_auth - logging in with predefined user

--- src/main/java/com/wordcloudwonders/controller/UserController.java
+++ src/main/java/com/wordcloudwonders/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.wordcloudwonders.controller;
 
 
+import com.wordcloudwonders.security.MyUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private MyUserService myUserService;
+
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    public String index(String data) {
+        System.out.println("data = " + data);
+        return "index";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        return "test";
+    }
 }
