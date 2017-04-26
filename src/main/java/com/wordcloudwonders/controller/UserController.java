8c2fa59d50f527481e6a971dcbb9b0242098227d
cickib <bellakocsis06@gmail.com>
2017-04-26 13:24:01 +0200

    user_auth - newly registered users saved to db

--- src/main/java/com/wordcloudwonders/controller/UserController.java
+++ src/main/java/com/wordcloudwonders/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.wordcloudwonders.controller;
 
 
+import com.wordcloudwonders.model.MyUser;
 import com.wordcloudwonders.security.MyUserService;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,8 +18,17 @@ public class UserController {
     private MyUserService myUserService;
 
     @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
-    public String index(String data) {
-        System.out.println("data = " + data);
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@RequestBody String data) throws JSONException {
+        MyUser newUser = new MyUser();
+        JSONObject json = new JSONObject(data);
+        newUser.setUsername(json.getString("username"));
+        newUser.setPassword(json.getString("password"));
+        myUserService.save(newUser);
         return "index";
     }
 
