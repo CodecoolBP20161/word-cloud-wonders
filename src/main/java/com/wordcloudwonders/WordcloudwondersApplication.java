1b83ea92f88d9c6dadf08c546c9cdda803813073
rekan <rekanagy06@gmail.com>
2017-04-27 11:36:26 +0200

    file_upload - create StorageProperties and update pom and main

--- src/main/java/com/wordcloudwonders/WordcloudwondersApplication.java
+++ src/main/java/com/wordcloudwonders/WordcloudwondersApplication.java
@@ -1,9 +1,12 @@
 package com.wordcloudwonders;
 
+import com.wordcloudwonders.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class WordcloudwondersApplication {
 
     public static void main(String[] args) {
