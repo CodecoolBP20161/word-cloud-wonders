d117ce2017696684970b149ae681441a7ed98387
rekan <rekanagy06@gmail.com>
2017-05-11 14:19:57 +0200

    testing - smoke test for application context

--- src/test/java/com/wordcloudwonders/WordcloudwondersApplicationTest.java
+++ src/test/java/com/wordcloudwonders/WordcloudwondersApplicationTest.java
@@ -1,16 +1,34 @@
 package com.wordcloudwonders;
 
+import com.wordcloudwonders.controller.FileUploadController;
+import com.wordcloudwonders.controller.UserController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class WordcloudwondersApplicationTests {
+public class WordcloudwondersApplicationTest {
+
+	@Autowired
+    private UserController userController;
+
+    @Autowired
+    private FileUploadController fileUploadController;
 
 	@Test
 	public void contextLoads() {
+        assertThat(userController).isNotNull();
+        assertThat(fileUploadController).isNotNull();
 	}
-
 }
