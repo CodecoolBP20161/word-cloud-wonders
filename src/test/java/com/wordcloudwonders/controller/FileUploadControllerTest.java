f39ef11e2de29cc218fd4ea211d27f815a2d2057
rekan <rekanagy06@gmail.com>
2017-05-12 11:19:10 +0200

    testing - test methods added to cover workflow

--- src/test/java/com/wordcloudwonders/controller/FileUploadControllerTest.java
+++ src/test/java/com/wordcloudwonders/controller/FileUploadControllerTest.java
@@ -1,5 +1,39 @@
-import static org.junit.Assert.*;
+package com.wordcloudwonders.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class FileUploadControllerTest {
+
+    @Test
+    public void GET_upload_shouldReturnUploadFormWithoutFiles() throws Exception {
+
+    }
+
+    @Test
+    public void POST_upload_shouldUploadFileCorrectType() throws Exception {
+
+    }
+
+    @Test
+    public void POST_upload_shouldNotUploadFileIncorrectType() throws Exception {
+
+    }
+
+    @Test
+    public void GET_upload_shouldReturnUploadFormWithFiles() throws Exception {
+
+    }
+
+    @Test
+    public void GET_fileByFileName_shouldReturnFileByFileName() throws Exception {
+
+    }
 
 }
